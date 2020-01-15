a16db23a0bc6e47f76e7cfd9fc5077af083b5656
Lex <domian844@gmail.com>
2020-01-15 14:45:20 +0700

    add space, chipsets, steam age items

--- src/main/java/com/gwppcore/gthandler/GT_Loader_Items.java
+++ src/main/java/com/gwppcore/gthandler/GT_Loader_Items.java
@@ -1,70 +1,76 @@
 package com.gwppcore.gthandler;
 
 import com.gwppcore.item.ItemList;
-import gregtech.common.items.GT_MetaGenerated_Item_01;
+import gregtech.common.items.GT_MetaGenerated_Item_04;
 
 public class GT_Loader_Items
 {
 	public void run()
 	{
-		GT = GT_MetaGenerated_Item_01.INSTANCE;
+		GT = GT_MetaGenerated_Item_04.INSTANCE;
 		registerItems();
 	}
 
-	private GT_MetaGenerated_Item_01 GT;
+	private GT_MetaGenerated_Item_04 GT;
 	
 	private void registerItems()
-	{	//LOGO GPWW
-		CustomItemList.Coin.set(ItemList.Coin.getIS());
-		//STEAM AGE
-		CustomItemList.WoodenBrickForm.set(ItemList.WoodenBrickForm.getIS());
-		CustomItemList.UnfiredSearedBrick.set(ItemList.UnfiredSearedBrick.getIS());
-		CustomItemList.UnfiredCokeOvenBrick.set(ItemList.UnfiredCokeOvenBrick.getIS());
-		CustomItemList.UnfiredClayBrick.set(ItemList.UnfiredClayBrick.getIS());
-		//CHIPSETS
-		CustomItemList.RedstoneRedChipset.set(ItemList.RedstoneRedChipset.getIS());
-		CustomItemList.RedstoneQuartzChipset.set(ItemList.RedstoneQuartzChipset.getIS());
-		CustomItemList.RedstonePulsatingChipset.set(ItemList.RedstonePulsatingChipset.getIS());
-		CustomItemList.RedstoneIronChipset.set(ItemList.RedstoneIronChipset.getIS());
-		CustomItemList.RedstoneGoldChipset.set(ItemList.RedstoneGoldChipset.getIS());
-		CustomItemList.RedstoneEmeraldChipset.set(ItemList.RedstoneEmeraldChipset.getIS());
-		CustomItemList.RedstoneDiamondChipset.set(ItemList.RedstoneDiamondChipset.getIS());
-		//SPACE RESEARCH
-		CustomItemList.packEuropa.set(ItemList.packEuropa.getIS());
-		CustomItemList.packBarnardaE.set(ItemList.packBarnardaE.getIS());
-		CustomItemList.packBarnardaF.set(ItemList.packBarnardaF.getIS());
-		CustomItemList.packCallisto.set(ItemList.packCallisto.getIS());
-		CustomItemList.packCentauriA.set(ItemList.packCentauriA.getIS());
-		CustomItemList.packCeres.set(ItemList.packCeres.getIS());
-		CustomItemList.packDeimos.set(ItemList.packDeimos.getIS());
-		CustomItemList.packEarth.set(ItemList.packEarth.getIS());
-		CustomItemList.packEris.set(ItemList.packEris.getIS());
-		CustomItemList.packGanymed.set(ItemList.packGanymed.getIS());
-		CustomItemList.packHaumea.set(ItemList.packHaumea.getIS());
-		CustomItemList.packIapetus.set(ItemList.packIapetus.getIS());
-		CustomItemList.packIo.set(ItemList.packIo.getIS());
-		CustomItemList.packMakeMake.set(ItemList.packMakeMake.getIS());
-		CustomItemList.packMercury.set(ItemList.packMercury.getIS());
-		CustomItemList.packMoon.set(ItemList.packMoon.getIS());
-		CustomItemList.packOberon.set(ItemList.packOberon.getIS());
-		CustomItemList.packPhobos.set(ItemList.packPhobos.getIS());
-		CustomItemList.packPluto.set(ItemList.packPluto.getIS());
-		CustomItemList.packTCetiE.set(ItemList.packTCetiE.getIS());
-		CustomItemList.packTitan.set(ItemList.packTitan.getIS());
-		CustomItemList.packTriton.set(ItemList.packTriton.getIS());
-		CustomItemList.packVegaB.set(ItemList.packVegaB.getIS());
-		CustomItemList.packVenus.set(ItemList.packVenus.getIS());
-		CustomItemList.packMars.set(ItemList.packMars.getIS());
-		CustomItemList.packProteus.set(ItemList.packProteus.getIS());
-		CustomItemList.packAsteroids.set(ItemList.packAsteroids.getIS());
-		CustomItemList.spacebox1.set(ItemList.spacebox1.getIS());
-		CustomItemList.spacebox2.set(ItemList.spacebox2.getIS());
-		CustomItemList.spacebox3.set(ItemList.spacebox3.getIS());
-		CustomItemList.spacebox4.set(ItemList.spacebox4.getIS());
-		CustomItemList.spacebox5.set(ItemList.spacebox5.getIS());
-		CustomItemList.spacebox6.set(ItemList.spacebox6.getIS());
-		CustomItemList.spacebox7.set(ItemList.spacebox7.getIS());
-		CustomItemList.spacebox8.set(ItemList.spacebox8.getIS());
+	{
+		/*=======================
+
+		 		ID 401++
+
+		=======================*/
+
+		CustomItemList.Coin.set(GT.addItem(						   401,                 "GWPP Coin",        "I`m is GOD",new Object[] {}));
+
+		CustomItemList.WoodenBrickForm.set(GT.addItem( 			   402,         "Wooden Brick Form",        "Reusable",new Object[]{}));
+		CustomItemList.UnfiredSearedBrick.set(GT.addItem( 		   403,      "Unfired Seared Brick",        "",new Object[]{}));
+		CustomItemList.UnfiredCokeOvenBrick.set(GT.addItem(	 	   404,   "Unfired Coke Oven Brick",        "",new Object[]{}));
+		CustomItemList.UnfiredClayBrick.set(GT.addItem( 		   405,        "Unfired Clay Brick",        "",new Object[]{}));
+		CustomItemList.RedstoneRedChipset.set(GT.addItem(          406,      "Redstone Red Chipset",        "",new Object[]{}));
+		CustomItemList.RedstoneQuartzChipset.set(GT.addItem(       407,   "Redstone Quartz Chipset",        "",new Object[]{}));
+		CustomItemList.RedstonePulsatingChipset.set(GT.addItem(    408,"Redstone Pulsating Chipset",        "",new Object[]{}));
+		CustomItemList.RedstoneIronChipset.set(GT.addItem(         409,     "Redstone Iron Chipset",        "",new Object[]{}));
+		CustomItemList.RedstoneGoldChipset.set(GT.addItem(         410,     "Redstone Gold Chipset",        "",new Object[]{}));
+		CustomItemList.RedstoneEmeraldChipset.set(GT.addItem(      411,  "Redstone Emerald Chipset",        "",new Object[]{}));
+		CustomItemList.RedstoneDiamondChipset.set(GT.addItem( 	   412,  "Redstone Diamond Chipset",        "",new Object[]{}));
+
+		CustomItemList.packEuropa.set(GT.addItem( 	               413,    "Pack Europa Stone Dust",        "",new Object[]{}));
+		CustomItemList.packBarnardaE.set(GT.addItem(               414, "Pack BarnardaE Stone Dust",        "",new Object[]{}));
+		CustomItemList.packBarnardaF.set(GT.addItem(               415, "Pack BarnardaF Stone Dust",        "",new Object[]{}));
+		CustomItemList.packCallisto.set(GT.addItem(	               416,  "Pack Callisto Stone Dust",        "",new Object[]{}));
+		CustomItemList.packCentauriA.set(GT.addItem(               417, "Pack CentauriA Stone Dust",        "",new Object[]{}));
+		CustomItemList.packCeres.set(GT.addItem( 	               418,     "Pack Ceres Stone Dust",        "",new Object[]{}));
+		CustomItemList.packDeimos.set(GT.addItem( 	               419,    "Pack Deimos Stone Dust",        "",new Object[]{}));
+		CustomItemList.packEarth.set(GT.addItem( 	               420,     "Pack Earth Stone Dust",        "",new Object[]{}));
+		CustomItemList.packEris.set(GT.addItem( 	               421,   "Pack Miranda Stone Dust",        "",new Object[]{}));
+		CustomItemList.packGanymed.set(GT.addItem( 	               422,   "Pack Ganymed Stone Dust",        "",new Object[]{}));
+		CustomItemList.packHaumea.set(GT.addItem( 	               423,    "Pack Haumea Stone Dust",        "",new Object[]{}));
+		CustomItemList.packIapetus.set(GT.addItem( 	               424, "Pack Enceladus Stone Dust",        "",new Object[]{}));
+		CustomItemList.packIo.set(GT.addItem(                      425,        "Pack Io Stone Dust",        "",new Object[]{}));
+		CustomItemList.packMakeMake.set(GT.addItem(                426, " Pack MakeMake Stone Dust",        "",new Object[]{}));
+		CustomItemList.packMercury.set(GT.addItem(                 427,   "Pack Mercury Stone Dust",        "",new Object[]{}));
+		CustomItemList.packProteus.set(GT.addItem(                 428,   "Pack Proteus Stone Dust",        "",new Object[]{}));
+		CustomItemList.packOberon.set(GT.addItem(                  429,    "Pack Oberon Stone Dust",        "",new Object[]{}));
+		CustomItemList.packPhobos.set(GT.addItem(                  430,    "Pack Phobos Stone Dust",        "",new Object[]{}));
+		CustomItemList.packPluto.set(GT.addItem(                   431,     "Pack Pluto Stone Dust",        "",new Object[]{}));
+		CustomItemList.packTCetiE.set(GT.addItem(                  432,    "Pack TCetiE Stone Dust",        "",new Object[]{}));
+		CustomItemList.packTitan.set(GT.addItem(                   433,     "Pack Titan Stone Dust",        "",new Object[]{}));
+		CustomItemList.packTriton.set(GT.addItem(                  434,    "Pack Triton Stone Dust",        "",new Object[]{}));
+		CustomItemList.packVegaB.set(GT.addItem(                   435,     "Pack VegaB Stone Dust",        "",new Object[]{}));
+		CustomItemList.packVenus.set(GT.addItem(                   436,     "Pack Venus Stone Dust",        "",new Object[]{}));
+		CustomItemList.packMoon.set(GT.addItem(                    437,      "Pack Moon Stone Dust",        "",new Object[]{}));
+		CustomItemList.packMars.set(GT.addItem(                    438,      "Pack Mars Stone Dust",        "",new Object[]{}));
+		CustomItemList.packAsteroids.set(GT.addItem(               439, "Pack Asteroids Stone Dust",        "",new Object[]{}));
+		CustomItemList.spacebox1.set(GT.addItem( 	               440, 		  "Space Box Tier 1",        "Use for crafting Rocket Tier-1",new Object[]{}));
+		CustomItemList.spacebox2.set(GT.addItem( 	               441, 		  "Space Box Tier 2",        "Use for crafting Rocket Tier-2",new Object[]{}));
+		CustomItemList.spacebox3.set(GT.addItem( 	               442, 		  "Space Box Tier 3",        "Use for crafting Rocket Tier-3",new Object[]{}));
+		CustomItemList.spacebox4.set(GT.addItem( 	               443, 		  "Space Box Tier 4",        "Use for crafting Rocket Tier-4",new Object[]{}));
+		CustomItemList.spacebox5.set(GT.addItem( 	               444, 		  "Space Box Tier 5",        "Use for crafting Rocket Tier-5",new Object[]{}));
+		CustomItemList.spacebox6.set(GT.addItem( 	               445, 		  "Space Box Tier 6",        "Use for crafting Rocket Tier-6",new Object[]{}));
+		CustomItemList.spacebox7.set(GT.addItem( 	               446, 		  "Space Box Tier 7",        "Use for crafting Rocket Tier-7",new Object[]{}));
+		CustomItemList.spacebox8.set(GT.addItem( 	               447, 		  "Space Box Tier 8",        "Use for crafting Rocket Tier-8",new Object[]{}));
+
 
 	}
 }
--- src/main/java/com/gwppcore/item/ItemList.java
+++ src/main/java/com/gwppcore/item/ItemList.java
@@ -9,60 +9,7 @@ import net.minecraft.item.ItemStack;
 
 public enum ItemList {
 
-	Coin(new ModSimpleBaseItem("Coin",ModTabList.GWppTab)),
-
-
-        //STEAM AGE
-    WoodenBrickForm(new ModSimpleBaseItem("WoodenBrickForm",ModTabList.GWppTab)),
-    UnfiredSearedBrick(new ModSimpleBaseItem("UnfiredSearedBrick",ModTabList.GWppTab)),
-    UnfiredCokeOvenBrick(new ModSimpleBaseItem("UnfiredCokeOvenBrick",ModTabList.GWppTab)),
-    UnfiredClayBrick(new ModSimpleBaseItem("UnfiredClayBrick",ModTabList.GWppTab)),
-
-        //BC CHIPS
-    RedstoneRedChipset(new ModSimpleBaseItem("RedstoneRedChipset",ModTabList.GWppTab)),
-    RedstoneQuartzChipset(new ModSimpleBaseItem("RedstoneQuartzChipset",ModTabList.GWppTab)),
-    RedstonePulsatingChipset(new ModSimpleBaseItem("RedstonePulsatingChipset",ModTabList.GWppTab)),
-    RedstoneIronChipset(new ModSimpleBaseItem("RedstoneIronChipset",ModTabList.GWppTab)),
-    RedstoneGoldChipset(new ModSimpleBaseItem("RedstoneGoldChipset",ModTabList.GWppTab)),
-    RedstoneEmeraldChipset(new ModSimpleBaseItem("RedstoneEmeraldChipset",ModTabList.GWppTab)),
-    RedstoneDiamondChipset(new ModSimpleBaseItem("RedstoneDiamondChipset",ModTabList.GWppTab)),
-
-        //SPACE RESEARCH
-    packEuropa(new ModSimpleBaseItem("packEuropa",ModTabList.GWppTab)),
-    packBarnardaE(new ModSimpleBaseItem("packBarnardaE",ModTabList.GWppTab)),
-    packBarnardaF(new ModSimpleBaseItem("packBarnardaF",ModTabList.GWppTab)),
-    packCallisto(new ModSimpleBaseItem("packCallisto",ModTabList.GWppTab)),
-    packCentauriA(new ModSimpleBaseItem("packCentauriA",ModTabList.GWppTab)),
-    packCeres(new ModSimpleBaseItem("packCeres",ModTabList.GWppTab)),
-    packDeimos(new ModSimpleBaseItem("packDeimos",ModTabList.GWppTab)),
-    packEarth(new ModSimpleBaseItem("packEarth",ModTabList.GWppTab)),
-    packEris(new ModSimpleBaseItem("packEris",ModTabList.GWppTab)),
-    packGanymed(new ModSimpleBaseItem("packGanymed",ModTabList.GWppTab)),
-    packHaumea(new ModSimpleBaseItem("packHaumea",ModTabList.GWppTab)),
-    packIapetus(new ModSimpleBaseItem("packIapetus",ModTabList.GWppTab)),
-    packIo(new ModSimpleBaseItem("packIo",ModTabList.GWppTab)),
-    packMakeMake(new ModSimpleBaseItem("packMakeMake",ModTabList.GWppTab)),
-    packMercury(new ModSimpleBaseItem("packMercury",ModTabList.GWppTab)),
-    packMoon(new ModSimpleBaseItem("packMoon",ModTabList.GWppTab)),
-    packOberon(new ModSimpleBaseItem("packOberon",ModTabList.GWppTab)),
-    packPhobos(new ModSimpleBaseItem("packPhobos",ModTabList.GWppTab)),
-    packPluto(new ModSimpleBaseItem("packPluto",ModTabList.GWppTab)),
-    packTCetiE(new ModSimpleBaseItem("packTCetiE",ModTabList.GWppTab)),
-    packTitan(new ModSimpleBaseItem("packTitan",ModTabList.GWppTab)),
-    packTriton(new ModSimpleBaseItem("packTriton",ModTabList.GWppTab)),
-    packVegaB(new ModSimpleBaseItem("packVegaB",ModTabList.GWppTab)),
-    packVenus(new ModSimpleBaseItem("packVenus",ModTabList.GWppTab)),
-    packMars(new ModSimpleBaseItem("packMars",ModTabList.GWppTab)),
-    packProteus(new ModSimpleBaseItem("packProteus",ModTabList.GWppTab)),
-    packAsteroids(new ModSimpleBaseItem("packAsteroids",ModTabList.GWppTab)),
-    spacebox1(new ModSimpleBaseItem("spacebox1",ModTabList.GWppTab)),
-    spacebox2(new ModSimpleBaseItem("spacebox2",ModTabList.GWppTab)),
-    spacebox3(new ModSimpleBaseItem("spacebox3",ModTabList.GWppTab)),
-    spacebox4(new ModSimpleBaseItem("spacebox4",ModTabList.GWppTab)),
-    spacebox5(new ModSimpleBaseItem("spacebox5",ModTabList.GWppTab)),
-    spacebox6(new ModSimpleBaseItem("spacebox6",ModTabList.GWppTab)),
-    spacebox7(new ModSimpleBaseItem("spacebox7",ModTabList.GWppTab)),
-    spacebox8(new ModSimpleBaseItem("spacebox8",ModTabList.GWppTab)),
+    Coin(new ModSimpleBaseItem("Coil",ModTabList.GWppTab)),
 
 	// Do not delete this
     EndOfList(null);
