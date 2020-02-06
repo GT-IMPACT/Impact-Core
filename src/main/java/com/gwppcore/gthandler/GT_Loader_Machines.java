a6d59ac17fb5cb46c32ffedb5dd3b3c22886a4ad
Lex <domian844@gmail.com>
2020-02-06 12:20:56 +0700

    add Multi Bender

--- src/main/java/com/gwppcore/gthandler/GT_Loader_Machines.java
+++ src/main/java/com/gwppcore/gthandler/GT_Loader_Machines.java
@@ -1,6 +1,7 @@
 package com.gwppcore.gthandler;
 
 
+import com.gwppcore.gthandler.tileentities.multi.GT_TileEntity_Bender;
 import com.gwppcore.gthandler.tileentities.multi.GT_TileEntity_ParallelBilder;
 import gregtech.api.util.GT_ModHandler;
 
@@ -15,8 +16,8 @@ public class GT_Loader_Machines {
 		long bitsd = GT_ModHandler.RecipeBits.DISMANTLEABLE | GT_ModHandler.RecipeBits.NOT_REMOVABLE
 				| GT_ModHandler.RecipeBits.REVERSIBLE | GT_ModHandler.RecipeBits.BUFFERED;
 
-		CustomItemList.Machine_TESTING_PARALLEL.set(new GT_TileEntity_ParallelBilder(14011, "multimachine.bendertest", "Machine_TESTING_PARALLEL").getStackForm(1L));
-
+		CustomItemList.Machine_TESTING_PARALLEL.set(new GT_TileEntity_ParallelBilder(14050, "multimachine.bendertest", "Machine_TESTING_PARALLEL").getStackForm(1L));
+		CustomItemList.Machine_TESTING_BENDER.set(new GT_TileEntity_Bender(14049, "multimachine.bender", "Multi Bending Machine").getStackForm(1L));
 	}
 
 }
