41987703c954b85b8a3c7c729e478f5d6bc4639f
Lex <domian844@gmail.com>
2020-03-16 21:07:04 +0700

    add Wire Factory

---
+++ src/main/java/com/gwppcore/GregTech/tileentities/multi/gui/GUI_Wire.java
@@ -0,0 +1,17 @@
+package com.gwppcore.GregTech.tileentities.multi.gui;
+
+import com.gwppcore.GregTech.tileentities.multi.GT_TileEntity_Wire;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import net.minecraft.entity.player.InventoryPlayer;
+
+public class GUI_Wire extends GUI_BASE {
+
+    public GUI_Wire(InventoryPlayer aInventoryPlayer, IGregTechTileEntity aTileEntity, String aName, String aTextureFile) {
+        super(aInventoryPlayer, aTileEntity, aName, aTextureFile);
+    }
+
+    @Override
+    String getmMode() {
+        return GT_TileEntity_Wire.mModed;
+    }
+}
