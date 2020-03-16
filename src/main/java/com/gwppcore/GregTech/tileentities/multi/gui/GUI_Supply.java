71e8265abc937350f50658f8f33dbaeab856a7a2
Lex <domian844@gmail.com>
2020-03-16 21:07:21 +0700

    add Supply Machine

---
+++ src/main/java/com/gwppcore/GregTech/tileentities/multi/gui/GUI_Supply.java
@@ -0,0 +1,17 @@
+package com.gwppcore.GregTech.tileentities.multi.gui;
+
+import com.gwppcore.GregTech.tileentities.multi.GT_TileEntity_Supply;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import net.minecraft.entity.player.InventoryPlayer;
+
+public class GUI_Supply extends GUI_BASE {
+
+    public GUI_Supply(InventoryPlayer aInventoryPlayer, IGregTechTileEntity aTileEntity, String aName, String aTextureFile) {
+        super(aInventoryPlayer, aTileEntity, aName, aTextureFile);
+    }
+
+    @Override
+    String getmMode() {
+        return GT_TileEntity_Supply.mModed;
+    }
+}
