d658e9e3ec79bb0ee460bdc2fc5d3ea9551cf964
Lex <domian844@gmail.com>
2020-04-02 03:44:59 +0700

    new textures Upgrade casing

---
+++ src/main/java/com/impact/recipes/machines/FreezSolidifierRecipe.java
@@ -0,0 +1,32 @@
+package com.impact.recipes.machines;
+
+import com.impact.mods.GregTech.GTregister.GT_Materials;
+import gregtech.api.enums.GT_Values;
+import gregtech.api.enums.ItemList;
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
+import gregtech.api.util.GT_OreDictUnificator;
+import net.minecraftforge.fluids.FluidRegistry;
+
+public class FreezSolidifierRecipe implements Runnable{
+    @Override
+    public void run(){
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.HastelloyC276.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.HastelloyC276, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.GumMetal.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.GumMetal, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Titaniolum.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Titaniolum, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Zamak.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Zamak, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Duraluminium.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Duraluminium, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Inconel690.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Inconel690, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Nitinol.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Nitinol, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Inconel792.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Inconel792, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.TiBetaC.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.TiBetaC, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.MaragingSteel250.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.MaragingSteel250, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Talonite.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Talonite, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Grisium.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Grisium, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Nitinol60.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Nitinol60, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Kovar.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Kovar, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Mangalloy.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Mangalloy, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.MaragingSteel300.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.MaragingSteel300, 1L), 20*20, 1920);
+        GT_Values.RA.addFreezerSolidifierRecipe(ItemList.Shape_Mold_Ingot.get(1L), FluidRegistry.getFluidStack("ic2coolant", 125), GT_Materials.Stellite.getMoltenHot(144L), GT_OreDictUnificator.get(OrePrefixes.ingot, GT_Materials.Stellite, 1L), 20*20, 1920);
+    }
+}
