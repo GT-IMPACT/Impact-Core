7385a5a8dd4ab07ea9bf656105f406402e99e99f
Lex <domian844@gmail.com>
2020-04-15 21:19:59 +0700

    add buffer casing for crafting

--- src/main/java/com/impact/loader/ItemRegistery.java
+++ src/main/java/com/impact/loader/ItemRegistery.java
@@ -1,5 +1,6 @@
 package com.impact.loader;
 
+import com.impact.block.BufferItem;
 import com.impact.block.Core_Blocks;
 import com.impact.block.Core_GlassBlocks;
 import com.impact.block.DecorateBlocksItem;
@@ -17,6 +18,7 @@ public class ItemRegistery {
 
     public static final Block[] decorateBlock = {
             new Core_Blocks("DecorateBlock", new String[] { Refstrings.MODID + ":blockConcrete", }, 0),
+            new Core_Blocks("BufferCasing", new String[] { Refstrings.MODID + ":bufferULV", Refstrings.MODID + ":bufferLV", Refstrings.MODID + ":bufferMV", Refstrings.MODID + ":bufferHV", Refstrings.MODID + ":bufferEV", Refstrings.MODID + ":bufferIV", Refstrings.MODID + ":bufferLuV", Refstrings.MODID + ":bufferZPM", Refstrings.MODID + ":bufferUV", Refstrings.MODID + ":bufferUHV"}, 1),
     };
 
     public static void run() {
@@ -24,17 +26,17 @@ public class ItemRegistery {
         for (int i=0; i<=15; i++) {
             GameRegistry.registerBlock(new Core_GlassBlocks("GlassBlock"+i, new String[]{ Refstrings.MODID + ":glass/blockGB"+i },null, true, true), GlassBlocksItem.class, "GlassBlock"+i);
         }
-
         GameRegistry.registerBlock(ItemRegistery.decorateBlock[0], DecorateBlocksItem.class, "DecorateBlock");
         OreDictionary.registerOre("concrete", new ItemStack(decorateBlock[0], 1, 0));
+        GameRegistry.registerBlock(ItemRegistery.decorateBlock[1], BufferItem.class, "BufferCasing");
     }
 
 
 
-    public static CircuitProgrammer toolCircuitProgrammer;
-    public static void CircuitProgrammer() {
-        toolCircuitProgrammer = new CircuitProgrammer();
-    }
+    //public static CircuitProgrammer toolCircuitProgrammer;
+    //public static void CircuitProgrammer() {
+    //    toolCircuitProgrammer = new CircuitProgrammer();
+    //}
 
     public static GregtechPump toolGregtechPump;
     public static void GregtechPump() {
