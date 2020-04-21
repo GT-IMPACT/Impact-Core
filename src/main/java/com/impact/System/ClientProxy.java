eb80f4d7b68dadf47c885530826cf2c4966ac6a4
Lex <domian844@gmail.com>
2020-04-21 23:38:36 +0700

    maximum refactor and del improt YAMCore

--- src/main/java/com/impact/System/ClientProxy.java
+++ src/main/java/com/impact/System/ClientProxy.java
@@ -1,32 +1,22 @@
 package com.impact.System;
 
-
-
 import cpw.mods.fml.client.FMLClientHandler;
-import cpw.mods.fml.client.registry.RenderingRegistry;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
-
-import static com.impact.System.ConfigHandler.CONFIG_HANDLER;
 
 public class ClientProxy extends CommonProxy {
 
-
 	@Override
     public void registerRenderInfo() {
-		MinecraftForge.EVENT_BUS.register(CONFIG_HANDLER);
     }
-	
+
 	@Override
-    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-    {
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
         return null;
     }
 
     @Override
-    public World getClientWorld()
-    {
+    public World getClientWorld() {
         return FMLClientHandler.instance().getClient().theWorld;
     }
 
--- src/main/java/com/impact/System/LoginHandler.java
+++ src/main/java/com/impact/System/LoginHandler.java
@@ -1,21 +1,22 @@
 package com.impact.System;
 
-import com.impact.System.CoreModConfig;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 
-public class LoginHandler
-{
+import static com.impact.impact.ModPackVersion;
+
+public class LoginHandler {
     @SuppressWarnings("unused")
     @SubscribeEvent
-    public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event)
-    {
+    public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "=================================================="));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD + "Welcome to IMPACT"/* + EnumChatFormatting.BOLD + EnumChatFormatting.GREEN + CoreModConfig.ModPackVersion*/) );
+        event.player.addChatMessage(new ChatComponentText(" "));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD + "Welcome to IMPACT "/* + ModPackVersion*/) );
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "This is a conversion patch #1") );
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BLUE + "Please bring comments to " + EnumChatFormatting.AQUA + "https://discord.gg/bMf2qvd" ));
+        event.player.addChatMessage(new ChatComponentText( " "));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "=================================================="));
     }
 }
