2a552a212c0eb13505c774f7861ea8e4c195654c
Lex <domian844@gmail.com>
2020-05-16 01:34:14 +0700

    del chestmod

--- src/main/java/com/impact/loader/GUIHandler.java
+++ src/main/java/com/impact/loader/GUIHandler.java
@@ -1,11 +1,5 @@
 package com.impact.loader;
 
-import com.impact.mods.GTSU.container.ContainerGTSU;
-import com.impact.mods.GTSU.gui.GuiGTSU;
-import com.impact.mods.GTSU.tileentity.TileEntityGTSU;
-import com.impact.mods.modChest.BASE.Container_BaseChest;
-import com.impact.mods.modChest.BASE.Gui_BaseChest;
-import com.impact.mods.modChest.BASE.TE_BaseChest;
 import com.impact.mods.modSolar.client.GuiAdvSolarPanel;
 import com.impact.mods.modSolar.common.TE.TileEntitySolarPanel;
 import cpw.mods.fml.common.network.IGuiHandler;
@@ -13,39 +7,38 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
+import static com.impact.System.impactLog.INFO;
+import static com.impact.System.impactLog.WARNING;
+
 public class GUIHandler implements IGuiHandler {
 
-    public static final int GUI_ID_GTSU = 0, GUI_ID_Chest = 1, GUI_ID_Solar = 2;
+    public static final int GUI_ID_Solar = 0;
 
     @Override
     public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
         final TileEntity te = world.getTileEntity(x, y, z);
-        if (te != null) {
-            switch (ID) {
-                case GUI_ID_GTSU:
-                    return new ContainerGTSU((TileEntityGTSU) te, player);
-                case GUI_ID_Chest:
-                    return new Container_BaseChest((TE_BaseChest) te, player.inventory);
-                case GUI_ID_Solar:
+        if (te == null) return null;
+        switch (ID) {
+            case GUI_ID_Solar:
+                if (te instanceof TileEntitySolarPanel)
                     return ((TileEntitySolarPanel) te).getGuiContainer(player.inventory);
-            }
+                INFO("GUIHandler Server - Loaded");
         }
+        WARNING("GUIHandler Server - Not Loaded");
         return null;
     }
 
     @Override
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
         TileEntity te = world.getTileEntity(x, y, z);
-        if (te != null) {
-            switch (ID) {
-                case GUI_ID_GTSU:
-                    return new GuiGTSU(new ContainerGTSU((TileEntityGTSU) te, player));
-                case GUI_ID_Chest:
-                    return new Gui_BaseChest((TE_BaseChest) te, player.inventory);
-                case GUI_ID_Solar:
+        if (te == null) return null;
+        switch (ID) {
+            case GUI_ID_Solar:
+                if (te instanceof TileEntitySolarPanel)
                     return new GuiAdvSolarPanel((TileEntitySolarPanel) te, player.inventory);
-            }
+                INFO("GUIHandler Client - Loaded");
         }
+        WARNING("GUIHandler Client - Not Loaded");
         return null;
     }
 }
