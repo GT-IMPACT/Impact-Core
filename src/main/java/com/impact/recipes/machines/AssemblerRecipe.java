0ffdd3e6f01b943d3b51efc88ef8e96bd5b14e3a
Спартак <spartak_korolev@mail.ru>
2020-07-12 14:38:09 +0300

    RC#2 1.0 - fix recipe of Maintenance Hatch

--- src/main/java/com/impact/recipes/machines/AssemblerRecipe.java
+++ src/main/java/com/impact/recipes/machines/AssemblerRecipe.java
@@ -211,7 +211,7 @@ public class AssemblerRecipe implements Runnable {
         GT_Values.RA.addAssemblerRecipe(new ItemStack[]{ItemList.Hull_MV.get(1L), ItemList.Electric_Motor_MV.get(2L), ItemList.Electric_Pump_MV.get(2L), GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Good, 2), GT_OreDictUnificator.get(OrePrefixes.gearGt, Materials.Steel, 2), GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Steel, 4), GT_OreDictUnificator.get(OrePrefixes.plateDouble, Materials.Steel, 8)}, GT_Values.NF, ItemList.OilDrill1.get(1L), 20 * 20, 120);
 
         //Maintenance Hatch
-        GT_Values.RA.addAssemblerRecipe(ItemList.Hull_LV.get(1), ItemList.Duct_Tape.get(8), GT_Values.NF, ItemList.Hatch_Maintenance.get(1L), 40 * 20, 30);
+        GT_Values.RA.addAssemblerRecipe(ItemList.Hull_LV.get(1), ItemList.Duct_Tape.get(4), GT_Values.NF, ItemList.Hatch_Maintenance.get(1L), 40 * 20, 30);
 
         /* ================================= end GT MOD =================================*/
 
