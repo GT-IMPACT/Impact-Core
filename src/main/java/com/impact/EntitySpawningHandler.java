79c88a543b9ab72cf67d1f65e06562e4c5c8b5e7
Lex <domian844@gmail.com>
2020-09-14 19:10:05 +0700

    RC#2 - mobs del fix imports
    - upd libs
    -fix imports

--- src/main/java/com/impact/EntitySpawningHandler.java
+++ src/main/java/com/impact/EntitySpawningHandler.java
@@ -1,10 +1,8 @@
 package com.impact;
 
 
-import cpw.mods.fml.common.eventhandler.Event;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import net.minecraft.entity.*;
-import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.EntityList;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
 import java.util.Arrays;
@@ -38,7 +36,7 @@ public class EntitySpawningHandler {
             "EvolvedSkeletonBoss",
             "AlienVillager",
             "CaveSpider"
-            );
+    );
 
     @SubscribeEvent
     public void onEntityJoinWorld(final EntityJoinWorldEvent event) {
