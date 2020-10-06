ddefcbc5996516e1d4c0a767d9fb58c6f353b3ea
Pilad <piladt@gmail.com>
2020-10-06 17:34:38 +0600

    Update MultiFluidHandler.java
    Лишний импорт.

--- src/main/java/com/impact/util/MultiFluidHandler.java
+++ src/main/java/com/impact/util/MultiFluidHandler.java
@@ -4,7 +4,6 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fluids.FluidStack;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MultiFluidHandler {
