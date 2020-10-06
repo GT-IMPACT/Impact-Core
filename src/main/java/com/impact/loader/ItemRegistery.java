696430e2200a7c8f51f5b524604b077f491a91c5
Lex <domian844@gmail.com>
2020-10-06 19:37:20 +0700

    fix imports

--- src/main/java/com/impact/loader/ItemRegistery.java
+++ src/main/java/com/impact/loader/ItemRegistery.java
@@ -1,5 +1,7 @@
 package com.impact.loader;
 
+import com.impact.common.block.itemblock.FakeBlocksItem;
+import com.impact.common.item.GT_Pump.GregtechPump;
 import com.impact.core.Refstrings;
 import com.impact.common.block.blocks.*;
 import cpw.mods.fml.common.registry.GameRegistry;
