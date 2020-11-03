914d45ad2b00359cd3a5affd4091ff922fe3dce6
Lex <domian844@gmail.com>
2020-11-03 18:14:44 +0700

    add Creative Tank

--- src/main/java/com/impact/mods/GregTech/Basic_Register.java
+++ src/main/java/com/impact/mods/GregTech/Basic_Register.java
@@ -1,5 +1,6 @@
 package com.impact.mods.GregTech;
 
+import com.impact.mods.GregTech.tileentities.basic.GTMTE_DebugTank;
 import com.impact.mods.GregTech.tileentities.basic.GTMTE_SemifluidGenerator;
 import com.impact.mods.GregTech.tileentities.basic.GTMTE_WaterTank;
 import com.impact.mods.GregTech.tileentities.hatches.*;
@@ -80,7 +81,10 @@ public class Basic_Register {
         Generator_Steam_Turbine_IV.set(new GT_MetaTileEntity_SteamTurbine(ID2++, "basicgenerator.steamturbine.tier.05", "IV Steam Turbine", 5).getStackForm(1L));
         Generator_Diesel_IV.set(new GT_MetaTileEntity_DieselGenerator(ID2++, "basicgenerator.diesel.tier.05", "IV Combustion Generator", 5).getStackForm(1L));
         Generator_Gas_Turbine_IV.set(new GT_MetaTileEntity_GasTurbine(ID2++, "basicgenerator.gasturbine.tier.05", "IV Gas Turbine", 5).getStackForm(1L));
-        Generator_Semi_Turbine_IV.set(new GTMTE_SemifluidGenerator(ID2, "basicgenerator.semifluid.tier.05", "IV Semifluid Generator", 5).getStackForm(1L));
+        Generator_Semi_Turbine_IV.set(new GTMTE_SemifluidGenerator(ID2++, "basicgenerator.semifluid.tier.05", "IV Semifluid Generator", 5).getStackForm(1L));
+
+        Creative_Tank.set(new GTMTE_DebugTank(ID2, "machine.creativetank", "Creative Tank", 5).getStackForm(1L));
+
 
 
         DustWasherULV.set(new GT_MetaTileEntity_BasicMachine_GT_Recipe(13035, "basicmachine.dustwasher.tier.00", "ULV Dust Washer", 0, "Washed your Dusts", GT_Recipe.GT_Recipe_Map.sDustWashRecipes, 1, 1, 8000, 0, 1, "Autoclave.png", "", false, false, 0, "DUSTWASHER", new Object[]{"IPI", "IMI", "ICI", 'I', OrePrefixes.plate.get(Materials.WroughtIron), 'P', GT_ItemList.ULVPump, 'M', GT_MetaTileEntity_BasicMachine_GT_Recipe.X.PIPE, 'C', GT_MetaTileEntity_BasicMachine_GT_Recipe.X.HULL}).getStackForm(1L));
