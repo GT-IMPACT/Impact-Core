08b2675c8e2ecaa46ea16eef4d9263b5d47efbc1
Lex <domian844@gmail.com>
2020-11-06 03:38:37 +0700

    @Spartak1997 check this
    Co-Authored-By: Спартак <spartak_korolev@mail.ru>

--- src/main/java/com/impact/recipes/RailCraftRecipe.java
+++ src/main/java/com/impact/recipes/RailCraftRecipe.java
@@ -1,40 +1,186 @@
 package com.impact.recipes;
 
+import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_ModHandler;
+import mods.railcraft.common.blocks.detector.EnumDetector;
+import mods.railcraft.common.blocks.machine.alpha.EnumMachineAlpha;
 import mods.railcraft.common.blocks.tracks.EnumTrack;
-import mods.railcraft.common.items.ItemRail;
-import mods.railcraft.common.items.ItemRailbed;
-import mods.railcraft.common.items.RailcraftItem;
+import mods.railcraft.common.core.RailcraftConfig;
+import mods.railcraft.common.items.*;
 import mods.railcraft.common.plugins.forge.CraftingPlugin;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 import static gregtech.api.util.GT_ModHandler.removeRecipe;
 import static gregtech.api.util.GT_ModHandler.removeRecipeByOutput;
+import static mods.railcraft.common.plugins.forge.CraftingPlugin.addShapelessRecipe;
 
 public class RailCraftRecipe implements Runnable {
 
     private static final long bits = GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE | GT_ModHandler.RecipeBits.DISMANTLEABLE | GT_ModHandler.RecipeBits.BUFFERED;
-
 
     public void delRecipe() {
         removeRecipeByOutput(EnumTrack.CONTROL.getItem());
     }
 
     public void handRecipe() {
+        Object[] recipe;
         // --- Control Track
-        final Object[] recipe = {
+        recipe = new Object[] {
                 "SRS", "ITI", "dRh",
                 'S', OrePrefixes.screw.get(Materials.Steel),
                 'R', OrePrefixes.stick.get(Materials.RedAlloy),
                 'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.ADVANCED),
                 'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
         };
-        GT_ModHandler.addCraftingRecipe(EnumTrack.CONTROL.getItem(8), recipe);
-
-
+        addShapelessRecipe(EnumTrack.CONTROL.getItem(8), recipe);
+        // --- Locking Track
+        recipe = new Object[] {
+                "SPS", "ITI", "dRh",
+                'S', OrePrefixes.screw.get(Materials.Steel),
+                'R', OrePrefixes.stick.get(Materials.RedAlloy),
+                'P', Blocks.stone_pressure_plate,
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.ADVANCED),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.LOCKING.getItem(8), recipe);
+        // --- Disembarkin Track
+        recipe = new Object[] {
+                "SPS", "ITI", "dRh",
+                'S', OrePrefixes.screw.get(Materials.Steel),
+                'R', OrePrefixes.stick.get(Materials.RedAlloy),
+                'P', EnumDetector.ADVANCED,
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.ADVANCED),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.DISEMBARK.getItem(8), recipe);
+        // --- Embarking Track
+        recipe = new Object[] {
+                "SRS", "ITI", "dPh",
+                'S', OrePrefixes.screw.get(Materials.Steel),
+                'R', OrePrefixes.stick.get(Materials.RedAlloy),
+                'P', EnumDetector.ADVANCED,
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.ADVANCED),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.EMBARKING.getItem(8), recipe);
+        // --- Coupler Track
+        recipe = new Object[] {
+                "SCS", "ITI", "dCh",
+                'S', OrePrefixes.screw.get(Materials.Steel),
+                'C', new ItemStack(new ItemCrowbarReinforced()),
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.ADVANCED),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.COUPLER.getItem(8), recipe);
+        // --- Whistle Track
+        recipe = new Object[] {
+                "SWS", "ITI", "dOh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'W', new ItemStack(new ItemWhistleTuner()),
+                'O', ItemList.Casing_Stripes_A,
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.WHISTLE.getItem(8), recipe);
+        // --- Locomotive Track
+        recipe = new Object[] {
+                "SOS", "ITI", "dOh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'O', RailcraftItem.signalLamp.getRecipeObject(),
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.LOCOMOTIVE.getItem(8), recipe);
+        // --- Limiter Track
+        recipe = new Object[] {
+                "SPS", "ITI", "dDh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'P', OrePrefixes.plate.get(Materials.RedAlloy),
+                'D', EnumDetector.ANY,
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.LIMITER.getItem(8), recipe);
+        // --- Routing Track
+        recipe = new Object[] {
+                "SPS", "ITI", "dDh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'P', OrePrefixes.plate.get(Materials.RedAlloy),
+                'D', new ItemStack(new ItemTicket()),
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.ROUTING.getItem(8), recipe);
+        recipe = new Object[] {
+                "SPS", "ITI", "dDh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'P', OrePrefixes.plate.get(Materials.RedAlloy),
+                'D', new ItemStack(new ItemTicketGold()),
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.ROUTING.getItem(8), recipe);
+        // --- Buffer Stop
+        recipe = new Object[] {
+                "SPS", "ITI", "dBh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'P', OrePrefixes.plate.get(Materials.Steel),
+                'B', OrePrefixes.block.get(Materials.Iron),
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.BUFFER_STOP.getItem(8), recipe);
+        // --- One Way Track
+        recipe = new Object[] {
+                "SPS", "ITI", "dRh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'P', Blocks.stone_pressure_plate,
+                'R', Blocks.piston,
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.ONEWAY.getItem(8), recipe);
+        // --- Directional Detector Track
+        recipe = new Object[] {
+                "SPS", "ITI", "dRh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'P', OrePrefixes.plate.get(Materials.RedAlloy),
+                'R', EnumDetector.ADVANCED,
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.DETECTOR_DIRECTION.getItem(8), recipe);
+        // --- Gated One Way Track
+        recipe = new Object[] {
+                "SRS", "ITI", "dRh",
+                'S', OrePrefixes.screw.get(Materials.Steel),
+                'R', GT_ModHandler.getModItem("minecraft", "fence", 1L),
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.ADVANCED),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.GATED_ONEWAY.getItem(8), recipe);
+        // --- Gated Track
+        recipe = new Object[] {
+                "SRS", "ITI", "dRh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'R', GT_ModHandler.getModItem("minecraft", "fence", 1L),
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.GATED.getItem(8), recipe);
+        // --- Suspended Rail
+        recipe = new Object[] {
+                "SRS", "ITI", "dRh",
+                'S', OrePrefixes.screw.get(Materials.Iron),
+                'R', GT_ModHandler.getModItem("minecraft", "fence", 1L),
+                'I', RailcraftItem.rail.getRecipeObject(ItemRail.EnumRail.STANDARD),
+                'T', RailcraftItem.railbed.getRecipeObject(ItemRailbed.EnumRailbed.WOOD)
+        };
+        addShapelessRecipe(EnumTrack.SUSPENDED.getItem(8), recipe);
     }
 
     @Override
