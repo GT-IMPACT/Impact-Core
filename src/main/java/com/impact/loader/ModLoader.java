19629664758d5ea83065daa1ccdf82ec6a9b2791
Lex <domian844@gmail.com>
2020-11-20 05:41:23 +0700

    fix Spartak Kosyachnik
    @Spartak1997  ты забыл джабба класс импортнуть, ну ты и косяк

--- src/main/java/com/impact/loader/ModLoader.java
+++ src/main/java/com/impact/loader/ModLoader.java
@@ -2,10 +2,7 @@ package com.impact.loader;
 
 import com.impact.mods.GregTech.TecTech.Holo_Impact;
 import com.impact.mods.GregTech.TecTech.Holo_Vanila_GregTech;
-import com.impact.recipes.OpenComputersRecipe;
-import com.impact.recipes.RailCraftRecipe;
-import com.impact.recipes.TecTechRecipe;
-import com.impact.recipes.StorageDrawersRecipe;
+import com.impact.recipes.*;
 import cpw.mods.fml.common.Loader;
 
 public class ModLoader implements Runnable {
