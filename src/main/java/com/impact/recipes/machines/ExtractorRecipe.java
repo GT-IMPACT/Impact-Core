001fcd5f9cf9ba5b0ff786128adf53e2e8f21e5f
Спартак <spartak_korolev@mail.ru>
2020-11-20 15:37:17 +0300

    Add more recipes for Unboxer
    - add recycling recipes for Crafting Storage
    - change recycling recipes of Energy Unit Block

--- src/main/java/com/impact/recipes/machines/ExtractorRecipe.java
+++ src/main/java/com/impact/recipes/machines/ExtractorRecipe.java
@@ -21,12 +21,5 @@ public class ExtractorRecipe implements Runnable {
         GT_Values.RA.addExtractorRecipe(CoreItems2.getRecipe(151, 64), CoreItems.getRecipe(35, 1), 400, 262144);
         GT_Values.RA.addExtractorRecipe(ItemList.Casing_CokeOvenBrick.get(1L), CoreItems2.getRecipe(65, 4), 300, 2);
         GT_Values.RA.addExtractorRecipe(GT_ModHandler.getModItem("TConstruct", "Smeltery", 1L, 2), GT_ModHandler.getModItem("TConstruct", "materials", 4L, 2), 300, 2);
-
-        GT_Values.RA.addExtractorRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 1), ItemList.Energy_LapotronicOrb.get(1L), 200, 7680);
-        GT_Values.RA.addExtractorRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 2), ItemList.Energy_LapotronicOrb2.get(1L), 300, 30720);
-        GT_Values.RA.addExtractorRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 3), ItemList.Energy_Module.get(1L), 400, 122880);
-        GT_Values.RA.addExtractorRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 4), ItemList.Energy_Cluster.get(1L), 500, 500000);
-        GT_Values.RA.addExtractorRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 5), ItemList.ZPM2.get(1L), 600, 2000000);
-
     }
 }
--- src/main/java/com/impact/recipes/machines/UnboxingRecipe.java
+++ src/main/java/com/impact/recipes/machines/UnboxingRecipe.java
@@ -3,7 +3,10 @@ package com.impact.recipes.machines;
 import com.impact.mods.GregTech.GT_ItemList;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.ItemList;
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_ModHandler;
+import gregtech.api.util.GT_OreDictUnificator;
 
 public class UnboxingRecipe implements Runnable {
     @Override
@@ -17,5 +20,16 @@ public class UnboxingRecipe implements Runnable {
         GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 6), ItemList.Cover_SolarPanel_LuV.get(1L), ItemList.Robot_Arm_LuV.get(1L), 2000, 1024);
         GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 7), ItemList.Cover_SolarPanel_ZPM.get(1L), ItemList.Robot_Arm_ZPM.get(1L), 2200, 4096);
         GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "BlockAdvSolarPanel", 1L, 8), ItemList.Cover_SolarPanel_UV.get(1L), ItemList.Robot_Arm_UV.get(1L), 2400, 7680);
+
+        GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("appliedenergistics2", "tile.BlockCraftingStorage", 1L, 0), GT_ModHandler.getModItem("appliedenergistics2", "tile.BlockCraftingUnit", 1L, 0), GT_ModHandler.getModItem("appliedenergistics2", "item.ItemMultiMaterial", 1L, 35), 50, 16);
+        GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("appliedenergistics2", "tile.BlockCraftingStorage", 1L, 1), GT_ModHandler.getModItem("appliedenergistics2", "tile.BlockCraftingUnit", 1L, 0), GT_ModHandler.getModItem("appliedenergistics2", "item.ItemMultiMaterial", 1L, 36), 50, 30);
+        GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("appliedenergistics2", "tile.BlockCraftingStorage", 1L, 2), GT_ModHandler.getModItem("appliedenergistics2", "tile.BlockCraftingUnit", 1L, 0), GT_ModHandler.getModItem("appliedenergistics2", "item.ItemMultiMaterial", 1L, 37), 50, 48);
+        GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("appliedenergistics2", "tile.BlockCraftingStorage", 1L, 3), GT_ModHandler.getModItem("appliedenergistics2", "tile.BlockCraftingUnit", 1L, 0), GT_ModHandler.getModItem("appliedenergistics2", "item.ItemMultiMaterial", 1L, 38), 50, 64);
+
+        GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 1), GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.Energy_LapotronicOrb.get(1L), 1600, 64);
+        GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 2), GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.Energy_LapotronicOrb2.get(1L), 1800, 256);
+        GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 3), GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.Energy_Module.get(1L), 2000, 1024);
+        GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 4), GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.Energy_Cluster.get(1L), 2200, 4096);
+        GT_Values.RA.addUnboxingRecipe(GT_ModHandler.getModItem("impact", "impact_lapotronicenergyunit_block", 1, 5), GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.ZPM2.get(1L), 2400, 7680);
     }
 }
