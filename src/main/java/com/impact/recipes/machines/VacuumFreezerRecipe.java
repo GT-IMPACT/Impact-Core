1d270bdeb7e1478be36b813f7aa5e4a7b5e065fa
Спартак <spartak_korolev@mail.ru>
2020-11-27 14:26:31 +0300

    Fix autogen recipes in EBF

--- src/main/java/com/impact/recipes/machines/VacuumFreezerRecipe.java
+++ src/main/java/com/impact/recipes/machines/VacuumFreezerRecipe.java
@@ -65,6 +65,8 @@ public class VacuumFreezerRecipe implements Runnable {
         GT_Values.RA.addVacuumFreezerRecipe(GT_OreDictUnificator.get(OrePrefixes.ingotHot, Materials.CosmicNeutronium, 1L), GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.CosmicNeutronium, 1L), 400, 122880);
         GT_Values.RA.addVacuumFreezerRecipe(GT_OreDictUnificator.get(OrePrefixes.ingotHot, Materials.InfinityCatalyst, 1L), GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.InfinityCatalyst, 1L), 500, 500000);
         GT_Values.RA.addVacuumFreezerRecipe(GT_OreDictUnificator.get(OrePrefixes.ingotHot, Materials.Infinity, 1L), GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.Infinity, 1L), 500, 2000000);
+        GT_Values.RA.addVacuumFreezerRecipe(GT_OreDictUnificator.get(OrePrefixes.ingotHot, Materials.Oriharukon, 1L), GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.Oriharukon, 1L), 603, 7680);
+        GT_Values.RA.addVacuumFreezerRecipe(GT_OreDictUnificator.get(OrePrefixes.ingotHot, Materials.Mytryl, 1L), GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.Mytryl, 1L), 576, 1920);
 
         GT_Values.RA.addVacuumFreezerRecipe(CoreItems2.getRecipe(70, 1), CoreItems2.getRecipe(69, 1), 400, 480);
         GT_Values.RA.addVacuumFreezerRecipe(GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.Ledox, 1L), CoreItems2.getRecipe(71, 1), 400, 480);
