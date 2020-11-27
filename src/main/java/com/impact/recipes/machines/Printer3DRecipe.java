c848a8c2dbd1aa0810c7d56821e64036184ded08
Спартак <spartak_korolev@mail.ru>
2020-11-27 03:14:53 +0300

    Add recipe for Energium Orb Capacitor

--- src/main/java/com/impact/recipes/machines/Printer3DRecipe.java
+++ src/main/java/com/impact/recipes/machines/Printer3DRecipe.java
@@ -163,31 +163,37 @@ public class Printer3DRecipe implements Runnable {
                 GT_OreDictUnificator.get(OrePrefixes.bolt, Materials.HSSE, 64), GT_OreDictUnificator.get(OrePrefixes.screw, Materials.HSSG, 64)
         }, GT_ItemList.LapPowerStation.get(1L), null, 200 * 20, 4096);
 
-        //LapatronicCapacitorIV
+        //LapotronicCapacitorIV
         GT_Values.RA.addBasicLineRecipe(new ItemStack[]{
                 GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.Energy_LapotronicOrb.get(1L), GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Platinum, 8), ItemList.Circuit_Chip_PIC.get(12L),
                 GT_OreDictUnificator.get(OrePrefixes.wireFine, Materials.TungstenSteel, 32), GT_OreDictUnificator.get(OrePrefixes.bolt, Materials.HastelloyC276, 64), GT_OreDictUnificator.get(OrePrefixes.screw, Materials.HSSG, 64)
         }, new ItemStack(lscLapotronicEnergyUnit, 1, 1), null, 100 * 20, 7680);
 
-        //LapatronicCapacitorLuV
+        //Energium Orb Capacitor
+        GT_Values.RA.addBasicLineRecipe(new ItemStack[]{
+                GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.EnergyCrystal_IV.get(1L), GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Platinum, 8), ItemList.Circuit_Chip_PIC.get(12L),
+                GT_OreDictUnificator.get(OrePrefixes.wireFine, Materials.TungstenSteel, 32), GT_OreDictUnificator.get(OrePrefixes.bolt, Materials.HastelloyC276, 64), GT_OreDictUnificator.get(OrePrefixes.screw, Materials.HSSG, 64)
+        }, new ItemStack(lscLapotronicEnergyUnit, 1, 6), null, 100 * 20, 7680);
+
+        //LapotronicCapacitorLuV
         GT_Values.RA.addBasicLineRecipe(new ItemStack[]{
                 GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.Energy_LapotronicOrb2.get(1L), GT_OreDictUnificator.get(OrePrefixes.foil, Materials.NiobiumTitanium, 8), ItemList.Circuit_Chip_HPIC.get(12L),
                 GT_OreDictUnificator.get(OrePrefixes.wireFine, Materials.YttriumBariumCuprate, 32), GT_OreDictUnificator.get(OrePrefixes.bolt, Materials.HastelloyN, 64), GT_OreDictUnificator.get(OrePrefixes.screw, Materials.HSSE, 64)
         }, new ItemStack(lscLapotronicEnergyUnit, 1, 2), null, 120 * 20, 30720);
 
-        //LapatronicCapacitorZPM
+        //LapotronicCapacitorZPM
         GT_Values.RA.addBasicLineRecipe(new ItemStack[]{
                 GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.Energy_Module.get(1L), GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Osmiridium, 8), ItemList.Circuit_Chip_UHPIC.get(12L),
                 GT_OreDictUnificator.get(OrePrefixes.wireFine, Materials.Naquadah, 32), GT_OreDictUnificator.get(OrePrefixes.bolt, Materials.Lafium, 64), GT_OreDictUnificator.get(OrePrefixes.screw, Materials.HSSS, 64)
         }, new ItemStack(lscLapotronicEnergyUnit, 1, 3), null, 140 * 20, 122880);
 
-        //LapatronicCapacitorUV
+        //LapotronicCapacitorUV
         GT_Values.RA.addBasicLineRecipe(new ItemStack[]{
                 GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.Energy_Cluster.get(1L), GT_OreDictUnificator.get(OrePrefixes.foil, Materials.NaquadahAlloy, 8), ItemList.Circuit_Chip_NPIC.get(12L),
                 GT_OreDictUnificator.get(OrePrefixes.wireFine, Materials.Duranium, 32), GT_OreDictUnificator.get(OrePrefixes.bolt, Materials.CinobiteA243, 64), GT_OreDictUnificator.get(OrePrefixes.screw, Materials.Tritanium, 64)
         }, new ItemStack(lscLapotronicEnergyUnit, 1, 4), null, 160 * 20, 500000);
 
-        //LapatronicCapacitorUHV
+        //LapotronicCapacitorUHV
         GT_Values.RA.addBasicLineRecipe(new ItemStack[]{
                 GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Inconel792, 1), ItemList.ZPM2.get(1L), GT_OreDictUnificator.get(OrePrefixes.foil, Materials.Americium, 8), ItemList.Circuit_Chip_PPIC.get(12L),
                 GT_OreDictUnificator.get(OrePrefixes.wireFine, Materials.Europium, 32), GT_OreDictUnificator.get(OrePrefixes.bolt, Materials.Pikyonium64B, 64), GT_OreDictUnificator.get(OrePrefixes.screw, Materials.Neutronium, 64)
