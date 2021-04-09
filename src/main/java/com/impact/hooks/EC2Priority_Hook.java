33608974b1a7e5ccc16e50dd858743cb66f00864
Alex Grishankov <domian844@gmail.com>
2021-04-09 21:07:19 +0700

    fix crash

--- src/main/java/com/impact/hooks/EC2Priority_Hook.java
+++ src/main/java/com/impact/hooks/EC2Priority_Hook.java
@@ -10,19 +10,4 @@ import net.minecraft.entity.player.EntityPlayer;
 
 public class EC2Priority_Hook {
 
-  @Hook(injectOnExit = true, isMandatory = true)
-  public static Object getClientGuiElement(PartFluidStorage part, EntityPlayer player) {
-    if (player.isSneaking()) {
-      return new GuiPriority(new ContainterPriority(player.inventory, part), player);
-    }
-    return new GuiBusFluidStorage(part, player);
-  }
-
-  @Hook(injectOnExit = true, isMandatory = true)
-  public static Object getServerGuiElement(PartFluidStorage part, EntityPlayer player) {
-    if (player.isSneaking()) {
-      return new ContainterPriority(player.inventory, part);
-    }
-    return new ContainerBusFluidStorage(part, player);
-  }
 }
