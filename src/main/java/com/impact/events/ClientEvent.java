bf2028bd0e81c160a14546c648e3014c399f6b75
Alex Grishankov <domian844@gmail.com>
2021-07-24 23:43:28 +0700

    detect cheats and crash

--- src/main/java/com/impact/events/ClientEvent.java
+++ src/main/java/com/impact/events/ClientEvent.java
@@ -25,94 +25,80 @@ import net.minecraftforge.client.event.MouseEvent;
 import org.lwjgl.input.Keyboard;
 
 public class ClientEvent {
-
-  @SubscribeEvent
-  @SideOnly(Side.CLIENT)
-  public void onGuiOpenEvent(GuiOpenEvent event) {
-    if (event.gui instanceof GuiMainMenu) {
-      event.gui = new ImpactGuiMainMenu();
-    }
-  }
-
-  @SideOnly(Side.CLIENT)
-  @SubscribeEvent
-  public void onMouseEvent(final MouseEvent event) {
-    final EntityPlayer entityPlayer = Minecraft.getMinecraft().thePlayer;
-    if (Keyboard.isKeyDown(Keyboard.KEY_RMENU) ||
-        Keyboard.isKeyDown(Keyboard.KEY_LMENU) ||
-        Keyboard.isKeyDown(Keyboard.KEY_RCONTROL) ||
-        Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
-      final ItemStack itemStack = entityPlayer.getHeldItem();
-      if (itemStack != null && itemStack.getItem() instanceof IB_IGlass) {
-        if (event.dwheel != 0) {
-          ImpactNetwork.INSTANCE.sendToServer(new ImpactPacketMetaDataPacket(
-              event.dwheel > 0, entityPlayer));
-        }
-        event.setCanceled(true);
-      }
-    }
-  }
-
-  @SideOnly(Side.CLIENT)
-  @SubscribeEvent
-  public void onKeyInput(InputEvent.KeyInputEvent event) {
-    if (KeyBindings.placeItem.isPressed()) {
-      EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
-      WorldClient world = Minecraft.getMinecraft().theWorld;
-      MovingObjectPosition mop = Utilits.raytraceFromEntity(world, player, 4.5D);
-      if (mop != null) {
-        ImpactNetwork.INSTANCE.sendToServer(new ImpactPacketPlacedItem(
-            (byte) mop.sideHit, mop.blockX, mop.blockY, mop.blockZ, player));
-      }
-    }
-  }
-
-  @SubscribeEvent
-  public void onDrawBlockHighlight(DrawBlockHighlightEvent aEvent) {
-    Error e = new Error();
-    e.setStackTrace(new StackTraceElement[]{});
-
-    try {
-      Class.forName("net.minecraftxray.loader.XRayForgeTweaker");
-      Minecraft.getMinecraft().crashed(new CrashReport("", e));
-      return;
-    } catch (Exception E) {
-    }
-
-    try {
-      Class.forName("de.Kradxn.Xray.mod_Xray");
-      Minecraft.getMinecraft().crashed(new CrashReport("", e));
-      return;
-    } catch (Exception E) {
-    }
-  }
-
-  //    @SideOnly(Side.CLIENT)
-//    @SubscribeEvent
-//    public void onClientTick(TickEvent.ClientTickEvent event) {
-//        Minecraft minecraft = Minecraft.getMinecraft();
-//        WorldClient world = minecraft.theWorld;
-//        if (world != null) {
-//
-//            //if (world.provider instanceof WorldProviderJupiter) {
-//            //    if (world.provider.getSkyRenderer() == null) {
-//            //        world.provider.setSkyRenderer((IRenderHandler) new SkyProviderJupiter());
-//            //    }
-////
-//            //    if (world.provider.getCloudRenderer() == null) {
-//            //        world.provider.setCloudRenderer((IRenderHandler) new CloudRendererJupiter());
-//            //    }
-//            //}
-//            //if (world.provider instanceof WorldProviderMars) {
-//            //    if (world.provider.getSkyRenderer() == null) {
-//            //        world.provider.setSkyRenderer(new SkyProviderMars((WorldProviderMars) world.provider));
-//            //    }
-////
-//            //    if (world.provider.getCloudRenderer() == null) {
-//            //        world.provider.setCloudRenderer(new CloudRenderer());
-//            //    }
-//            //}
-//        }
-//    }
-
+	
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onGuiOpenEvent(GuiOpenEvent event) {
+		if (event.gui instanceof GuiMainMenu) {
+			event.gui = new ImpactGuiMainMenu();
+		}
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public void onMouseEvent(final MouseEvent event) {
+		final EntityPlayer entityPlayer = Minecraft.getMinecraft().thePlayer;
+		if (Keyboard.isKeyDown(Keyboard.KEY_RMENU) ||
+				Keyboard.isKeyDown(Keyboard.KEY_LMENU) ||
+				Keyboard.isKeyDown(Keyboard.KEY_RCONTROL) ||
+				Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
+			final ItemStack itemStack = entityPlayer.getHeldItem();
+			if (itemStack != null && itemStack.getItem() instanceof IB_IGlass) {
+				if (event.dwheel != 0) {
+					ImpactNetwork.INSTANCE.sendToServer(new ImpactPacketMetaDataPacket(
+							event.dwheel > 0, entityPlayer));
+				}
+				event.setCanceled(true);
+			}
+		}
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public void onKeyInput(InputEvent.KeyInputEvent event) {
+		if (KeyBindings.placeItem.isPressed()) {
+			EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
+			WorldClient world = Minecraft.getMinecraft().theWorld;
+			MovingObjectPosition mop = Utilits.raytraceFromEntity(world, player, 4.5D);
+			if (mop != null) {
+				ImpactNetwork.INSTANCE.sendToServer(new ImpactPacketPlacedItem(
+						(byte) mop.sideHit, mop.blockX, mop.blockY, mop.blockZ, player));
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onDrawBlockHighlight(DrawBlockHighlightEvent aEvent) {
+		Error e = new Error();
+		e.setStackTrace(new StackTraceElement[]{});
+		
+		try {
+			Class.forName("net.minecraftxray.loader.XRayForgeTweaker");
+			Minecraft.getMinecraft().crashed(new CrashReport("", e));
+			return;
+		} catch (Exception ignored) {
+		}
+		
+		try {
+			Class.forName("de.Kradxn.Xray.mod_Xray");
+			Minecraft.getMinecraft().crashed(new CrashReport("", e));
+			return;
+		} catch (Exception ignored) {
+		}
+		
+		try {
+			Class.forName("forgefuck.team.xenobyte.ModulesList");
+			Minecraft.getMinecraft().crashed(new CrashReport("", e));
+			return;
+		} catch (Exception ignored) {
+		}
+		
+		try {
+			Class.forName("forgefuck.team.xenobyte.XenoByte");
+			Minecraft.getMinecraft().crashed(new CrashReport("", e));
+			return;
+		} catch (Exception ignored) {
+		}
+		System.out.println("No detect cheats");
+	}
 }
