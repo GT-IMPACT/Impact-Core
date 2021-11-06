75e596c3bc27ae839176e962d2e17bc66ed4c56d
4gname <domian844@gmail.com>
2021-11-06 21:25:23 +0700

    fix energy LDPL

--- src/main/java/com/impact/mods/gregtech/tileentities/basic/GTMTE_LongDistancePipelineEnergy.java
+++ src/main/java/com/impact/mods/gregtech/tileentities/basic/GTMTE_LongDistancePipelineEnergy.java
@@ -10,6 +10,7 @@ import gregtech.api.objects.GT_RenderedTexture;
 import gregtech.api.util.GT_Utility;
 
 import static gregtech.api.enums.GT_Values.V;
+import static gregtech.api.util.GT_Utility.getOppositeSide;
 
 public class GTMTE_LongDistancePipelineEnergy extends GTMTE_LongDistancePipelineBase {
 	
@@ -37,26 +38,23 @@ public class GTMTE_LongDistancePipelineEnergy extends GTMTE_LongDistancePipeline
 	}
 	
 	@Override
-	public void onPostTick(IGregTechTileEntity te, long aTick) {
-		super.onPostTick(te, aTick);
+	public void onPostTick(IGregTechTileEntity inTE, long aTick) {
+		super.onPostTick(inTE, aTick);
 		
-		if (te.isServerSide() && checkTarget()) {
-			final IGregTechTileEntity tTile = mTarget.getBaseMetaTileEntity();
+		if (inTE.isServerSide() && checkTarget()) {
+			final IGregTechTileEntity outTE = mTarget.getBaseMetaTileEntity();
 			
-			IGregTechTileEntity teDest = te.getIGregTechTileEntityAtSide(te.getFrontFacing());
-			IGregTechTileEntity teTarget = tTile.getIGregTechTileEntityAtSide(tTile.getBackFacing());
-			if (teDest != null && teTarget != null) {
+			IGregTechTileEntity in = inTE.getIGregTechTileEntityAtSide(inTE.getFrontFacing());
+			IGregTechTileEntity out = outTE.getIGregTechTileEntityAtSide(outTE.getBackFacing());
+			
+			if (in != null && out != null) {
 				long tEU = V[mTier];
-				long tAmp = 1;
-				if (teDest.inputEnergyFrom(te.getFrontFacing()) && teTarget.outputsEnergyTo(tTile.getBackFacing())) {
-					if (teTarget.getStoredEU() < teTarget.getEUCapacity()) {
-						tAmp = teTarget.injectEnergyUnits((byte) 6, tEU, teDest.getOutputAmperage());
-						teDest.drainEnergyUnits((byte) 6, tEU, tAmp);
-					}
-				} else if (teDest.outputsEnergyTo(tTile.getFrontFacing()) && teTarget.inputEnergyFrom(te.getBackFacing())) {
-					if (teDest.getStoredEU() < teDest.getEUCapacity()) {
-						tAmp = teDest.injectEnergyUnits((byte) 6, tEU, teTarget.getOutputAmperage());
-						teTarget.drainEnergyUnits((byte) 6, tEU, tAmp);
+				if (inTE.getFrontFacing() == getOppositeSide(in.getFrontFacing()) && outTE.getBackFacing() != getOppositeSide(out.getFrontFacing())) {
+					if (in.isUniversalEnergyStored(tEU)) {
+						long tAmp = out.injectEnergyUnits((byte) 6, tEU, in.getOutputAmperage());
+						if (tAmp > 0) {
+							in.drainEnergyUnits((byte) 6, tEU, tAmp);
+						}
 					}
 				}
 			}
@@ -76,7 +74,7 @@ public class GTMTE_LongDistancePipelineEnergy extends GTMTE_LongDistancePipeline
 	public ITexture[] getTexture(IGregTechTileEntity te, byte aSide, byte aFacing, byte aColorIndex, boolean aActive, boolean aRedstone) {
 		if (aSide == aFacing) {
 			return new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[mTier][aColorIndex + 1], Textures.BlockIcons.OVERLAYS_ENERGY_IN_MULTI[mTier]};
-		} else if (aSide == GT_Utility.getOppositeSide(aFacing)) {
+		} else if (aSide == getOppositeSide(aFacing)) {
 			return new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[mTier][aColorIndex + 1], Textures.BlockIcons.OVERLAYS_ENERGY_OUT_MULTI[mTier]};
 		} else {
 			return new ITexture[]{Textures.BlockIcons.MACHINE_CASINGS[mTier][aColorIndex + 1], new GT_RenderedTexture(Texture.Icons.OVERLAY_PIPELINE_ENERGY_SIDE)};
