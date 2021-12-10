afe89c33fec07a1d07e1064340601c1d92792cf9
4gname <domian844@gmail.com>
2021-12-10 15:12:20 +0700

    Update Mixin.java

--- src/main/java/com/impact/mixinplugin/Mixin.java
+++ src/main/java/com/impact/mixinplugin/Mixin.java
@@ -14,12 +14,10 @@ public enum Mixin {
 	// you refer to other mods you load them as well. The consequence is: You can't inject any previously loaded classes!
 	// Exception: Tags.java, as long as it is used for Strings only!
 	//
+
+//	GT_Block_Ores_AbstractMixin("gregtech.GT_Block_Ores_AbstractMixin", GREGTECH, VANILLA);
 	
-	// Replace with your own mixins:
-	ItemEditableBookMixin("minecraft.ItemEditableBookMixin", Side.BOTH, VANILLA),
-	// You may also require multiple mods to be loaded if your mixin requires both
-	GT_Block_Ores_AbstractMixin("gregtech.GT_Block_Ores_AbstractMixin", GREGTECH, VANILLA);
-	
+	;
 	public final String mixinClass;
 	public final List<TargetedMod> targetedMods;
 	private final Side side;
