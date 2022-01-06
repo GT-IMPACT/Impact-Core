238a164613e4b86548d0c3c9fb0c5084aa73f0bc
4gname <domian844@gmail.com>
2022-01-07 00:48:48 +0700

    fix Ore Probe lang

--- src/main/java/com/impact/mods/gregtech/items/tools/behaviour/Behaviour_OreProbe.java
+++ src/main/java/com/impact/mods/gregtech/items/tools/behaviour/Behaviour_OreProbe.java
@@ -3,16 +3,21 @@ package com.impact.mods.gregtech.items.tools.behaviour;
 import com.impact.common.oregeneration.OreGenerator;
 import com.impact.common.oregeneration.OreVein;
 import com.impact.util.Utilits;
+import gregtech.api.enums.Materials;
+import gregtech.api.enums.OrePrefixes;
 import gregtech.api.interfaces.IItemBehaviour;
 import gregtech.api.items.GT_MetaBase_Item;
+import gregtech.api.util.GT_LanguageManager;
 import gregtech.api.util.GT_Utility;
 import gregtech.common.items.behaviors.Behaviour_None;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class Behaviour_OreProbe extends Behaviour_None {
 	
@@ -24,8 +29,9 @@ public class Behaviour_OreProbe extends Behaviour_None {
 			OreVein oreVein = OreGenerator.getOreVein(ch, 0);
 			if (oreVein != null) {
 				int countOres = oreVein.ores.size();
-				String name = oreVein.ores.get(Utilits.getRandom(0, countOres - 1)).getDisplayName();
-				GT_Utility.sendChatToPlayer(aPlayer, "Presumably there is Ore here: " + EnumChatFormatting.YELLOW + name);
+				ItemStack is = oreVein.ores.get(Utilits.getRandom(0, countOres - 1));
+				
+				GT_Utility.sendChatToPlayer(aPlayer, "Presumably there is Ore here: " + EnumChatFormatting.YELLOW + Utilits.translateGTItemStack(is));
 			} else {
 				GT_Utility.sendChatToPlayer(aPlayer, "Not found Ores");
 			}
