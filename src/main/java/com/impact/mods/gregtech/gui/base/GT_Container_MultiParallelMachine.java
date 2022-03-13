281e51743c50da54788af3879efb4ca8721ba22d
4gname <domian844@gmail.com>
2022-03-13 11:13:04 +0700

    refactor space elevator

--- src/main/java/com/impact/mods/gregtech/gui/base/GT_Container_MultiParallelMachine.java
+++ src/main/java/com/impact/mods/gregtech/gui/base/GT_Container_MultiParallelMachine.java
@@ -11,42 +11,41 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
 public class GT_Container_MultiParallelMachine extends GT_ContainerMetaTile_Machine {
-
-  GT_MetaTileEntity_MultiBlockBase mte;
-
-  public GT_Container_MultiParallelMachine(InventoryPlayer aInventoryPlayer, IGregTechTileEntity aTileEntity) {
-    super(aInventoryPlayer, aTileEntity);
-    mte = (GT_MetaTileEntity_MultiBlockBase) mTileEntity.getMetaTileEntity();
-  }
-
-  public GT_Container_MultiParallelMachine(InventoryPlayer aInventoryPlayer, IGregTechTileEntity aTileEntity, GT_MetaTileEntity_MultiBlockBase base) {
-    super(aInventoryPlayer, aTileEntity);
-    mte = base;
-  }
-
-  @Override
-  public void addSlots(InventoryPlayer aInventoryPlayer) {
-    addSlotToContainer(new GT_Slot_Holo(mTileEntity, 2, 152, 62, false, false, 1));
-  }
-
-  @Override
-  public ItemStack slotClick(int aSlotIndex, int aMouseclick, int aShifthold, EntityPlayer aPlayer) {
-    if (aSlotIndex < 0 || aSlotIndex > 2) {
-      return super.slotClick(aSlotIndex, aMouseclick, aShifthold, aPlayer);
-    }
-    Slot tSlot = (Slot) inventorySlots.get(aSlotIndex);
-    if (tSlot != null && mTileEntity.getMetaTileEntity() != null) {
-      if (aSlotIndex == 0) {
-        if (mte.getBaseMetaTileEntity().isAllowedToWork()) {
-          GT_Utility.sendChatToPlayer(aPlayer, "Machine Processing: Disabled");
-          mte.getBaseMetaTileEntity().disableWorking();
-        } else {
-          GT_Utility.sendChatToPlayer(aPlayer, "Machine Processing: Enabled");
-          mte.getBaseMetaTileEntity().enableWorking();
-        }
-      }
-    }
-    return super.slotClick(aSlotIndex, aMouseclick, aShifthold, aPlayer);
-  }
-
+	
+	protected GT_MetaTileEntity_MultiBlockBase mte;
+	
+	public GT_Container_MultiParallelMachine(InventoryPlayer aInventoryPlayer, IGregTechTileEntity aTileEntity) {
+		super(aInventoryPlayer, aTileEntity);
+		mte = (GT_MetaTileEntity_MultiBlockBase) mTileEntity.getMetaTileEntity();
+	}
+	
+	public GT_Container_MultiParallelMachine(InventoryPlayer aInventoryPlayer, IGregTechTileEntity aTileEntity, GT_MetaTileEntity_MultiBlockBase base) {
+		super(aInventoryPlayer, aTileEntity);
+		mte = base;
+	}
+	
+	@Override
+	public void addSlots(InventoryPlayer aInventoryPlayer) {
+		addSlotToContainer(new GT_Slot_Holo(mTileEntity, 2, 152, 62, false, false, 1));
+	}
+	
+	@Override
+	public ItemStack slotClick(int aSlotIndex, int aMouseclick, int aShifthold, EntityPlayer aPlayer) {
+		if (aSlotIndex < 0 || aSlotIndex > 2) {
+			return super.slotClick(aSlotIndex, aMouseclick, aShifthold, aPlayer);
+		}
+		Slot tSlot = (Slot) inventorySlots.get(aSlotIndex);
+		if (tSlot != null && mTileEntity.getMetaTileEntity() != null) {
+			if (aSlotIndex == 0) {
+				if (mte.getBaseMetaTileEntity().isAllowedToWork()) {
+					GT_Utility.sendChatToPlayer(aPlayer, "Machine Processing: Disabled");
+					mte.getBaseMetaTileEntity().disableWorking();
+				} else {
+					GT_Utility.sendChatToPlayer(aPlayer, "Machine Processing: Enabled");
+					mte.getBaseMetaTileEntity().enableWorking();
+				}
+			}
+		}
+		return super.slotClick(aSlotIndex, aMouseclick, aShifthold, aPlayer);
+	}
 }
