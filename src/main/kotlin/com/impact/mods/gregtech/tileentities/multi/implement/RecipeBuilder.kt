b2dfcb866c758d5d38ea26fc5495da428b5ea7cc
4gname <domian844@gmail.com>
2022-04-10 16:51:55 +0700

    fix critical bug with multis
    fix https://github.com/GT-IMPACT/IMPACT-MODPACK/issues/528
    fix https://github.com/GT-IMPACT/IMPACT-MODPACK/issues/542
    fix https://github.com/GT-IMPACT/IMPACT-MODPACK/issues/536

--- src/main/kotlin/com/impact/mods/gregtech/tileentities/multi/implement/RecipeBuilder.kt
+++ src/main/kotlin/com/impact/mods/gregtech/tileentities/multi/implement/RecipeBuilder.kt
@@ -1,14 +1,9 @@
 package com.impact.mods.gregtech.tileentities.multi.implement
 
-import gregtech.api.enums.ItemList
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_Hatch
 import gregtech.api.util.GT_Recipe
 
 object RecipeBuilder {
-
-    @JvmStatic
-    fun bussesIsNoEmpty(hatches: List<GT_MetaTileEntity_Hatch>): Boolean =
-        hatches.any { it.mInventory.any { stack -> stack != null && stack.item != ItemList.Display_Fluid.item } }
 
     @JvmStatic
     fun <MULTIS : GT_MetaTileEntity_MultiParallelBlockBase<MULTIS>> check3DPrinterRecipe(multis: MULTIS): Boolean {
@@ -19,23 +14,16 @@ object RecipeBuilder {
             addAll(multis.mInputBusHatches)
             addAll(multis.mInputHatches)
         }
-
-        val checkItemHatches = (multis.mInputBusses.isNotEmpty() || multis.mInputBusHatches.isNotEmpty())
-        val separation = checkItemHatches && multis.modeBuses == 0
-        val preCheck = (bussesIsNoEmpty(busses) || !checkItemHatches)
-        
-        if (!preCheck) return false
-
         var recipe: MultiBlockRecipe<MULTIS>
         var gtRecipe: GT_Recipe?
         for (mInputBuss: GT_MetaTileEntity_Hatch in busses) {
             recipe = MultiBlockRecipe(multis)
             recipe.apply {
-                sortItems(if (separation) mInputBuss else null)
+                sortItems(if (multis.modeBuses == 0) mInputBuss else null)
                 sortFluids()
                 checkSizeHatches()
                 checkVoltage(vanila = true)
-                gtRecipe = checkRecipeMap()
+                gtRecipe = checkRecipeMap(aDontCheckStackSizes = false)
                 gtRecipe?.apply {
                     checkInputEquals(recipe = this)
                     setEfficiency()
@@ -60,19 +48,12 @@ object RecipeBuilder {
             addAll(multis.mInputBusHatches)
             addAll(multis.mInputHatches)
         }
-
-        val checkItemHatches = (multis.mInputBusses.isNotEmpty() || multis.mInputBusHatches.isNotEmpty())
-        val separation = checkItemHatches && multis.modeBuses == 0
-        val preCheck = (bussesIsNoEmpty(busses) || !checkItemHatches)
-
-        if (!preCheck) return false
-
         var recipe: MultiBlockRecipe<MULTIS>
         var gtRecipe: GT_Recipe?
         for (mInputBuss: GT_MetaTileEntity_Hatch in busses) {
             recipe = MultiBlockRecipe(multis)
             recipe.apply {
-                sortItems(if (separation) mInputBuss else null)
+                sortItems(if (multis.modeBuses == 0) mInputBuss else null)
                 sortFluids()
                 checkSizeHatches()
                 checkVoltage(vanila = true)
@@ -105,22 +86,16 @@ object RecipeBuilder {
             addAll(multis.mInputBusHatches)
             addAll(multis.mInputHatches)
         }
-        val checkItemHatches = (multis.mInputBusses.isNotEmpty() || multis.mInputBusHatches.isNotEmpty())
-        val separation = checkItemHatches && multis.modeBuses == 0
-        val preCheck = (bussesIsNoEmpty(busses) || !checkItemHatches)
-        if (!preCheck) return false
-
         if (multis.sParallHatchesIn.isNotEmpty() && !multis.mRecipeCheckParallel) {
             return false
         }
-
         var recipe: MultiBlockRecipe<MULTIS>
         var gtRecipe: GT_Recipe?
 
         for (mInputBuss: GT_MetaTileEntity_Hatch in busses) {
             recipe = MultiBlockRecipe(multis)
             recipe.apply {
-                sortItems(if (separation) mInputBuss else null)
+                sortItems(if (multis.modeBuses == 0) mInputBuss else null)
                 sortFluids()
                 checkSizeHatches()
                 checkVoltage(vanila = false)
