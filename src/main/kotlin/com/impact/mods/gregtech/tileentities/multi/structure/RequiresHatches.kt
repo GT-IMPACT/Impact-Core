c7764f6f3cf4d82a1c419bd995a65fc59f1b3339
AlexeyGrishankov <domian844@gmail.com>
2023-05-29 23:30:18 +0300

    add multis hatch require hack

---
+++ src/main/kotlin/com/impact/mods/gregtech/tileentities/multi/structure/RequiresHatches.kt
@@ -0,0 +1,41 @@
+package com.impact.mods.gregtech.tileentities.multi.structure
+
+import com.impact.mods.gregtech.tileentities.multi.implement.GTMTE_Impact_BlockBase
+import com.impact.mods.gregtech.tileentities.multi.implement.GT_MetaTileEntity_MultiParallelBlockBase
+
+fun GTMTE_Impact_BlockBase<*>.hasRequireHatches(
+    energy: Int = 0,
+    dynamo: Int = 0,
+    inputBuss: Int = 0,
+    outputBuss: Int = 0,
+    inputHatch: Int = 0,
+    outputHatch: Int = 0,
+    maintenance: Int = 0,
+    muffler: Int = 0,
+): Boolean {
+    return mEnergyHatches.size + mEnergyHatchesMulti.size + mLaserIn.size + mLaserOut.size <= energy &&
+            mDynamoHatches.size + mDynamoHatchesMulti.size <= dynamo &&
+            mInputBusses.size + mInputBusHatches.size <= inputBuss &&
+            mOutputBusses.size <= outputBuss &&
+            mInputHatches.size + mQuadrInputHatches.size <= inputHatch &&
+            mOutputHatches.size <= outputHatch &&
+            mMaintenanceHatches.size <= maintenance &&
+            mMufflerHatches.size <= muffler
+}
+
+fun GT_MetaTileEntity_MultiParallelBlockBase<*>.hasRequireHatches(
+    energy: Int = 0,
+    dynamo: Int = 0,
+    inputBuss: Int = 0,
+    outputBuss: Int = 0,
+    inputHatch: Int = 0,
+    outputHatch: Int = 0,
+    maintenance: Int = 0,
+    muffler: Int = 0,
+    parallelIn: Int = 0,
+    parallelOut: Int = 0,
+): Boolean {
+    return hasRequireHatches(
+        energy, dynamo, inputBuss, outputBuss, inputHatch, outputHatch, maintenance, muffler
+    ) && sParallHatchesIn.size <= parallelIn && sParallHatchesOut.size <= parallelOut
+}
