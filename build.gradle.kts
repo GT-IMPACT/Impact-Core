7af02f06557f828a5fee1e9d04b947a361caed8e
Grishankov Alexey <domian844@gmail.com>
2026-06-01 08:44:46 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ repositories {
     maven("https://maven.accident.space/repository/maven-public/") {
         mavenContent {
             includeGroup("space.impact")
+            includeGroup("com.github.GTNewHorizons")
+            includeGroup("com.github.Azanor")
             includeGroupByRegex("space\\.impact\\..+")
         }
         credentials {
@@ -37,24 +39,29 @@ repositories {
 }
 
 dependencies {
+    // impact
     api("space.impact:Packet-network:1.1.8.dirty")
     api("space.impact:ImpactAPI:0.0.4:dev")
     api("space.impact:WAILAPlugins:0.3.1.dirty:dev")
-    api("space.impact:VirtualWorld:2.0.0b.dirty:dev")
-    api("space.impact:gregtech-impact:5.09.35.28.dirty:dev") { isTransitive = false }
-//    api("com.github.GTNewHorizons:ModularUI:1.1.24:dev") { isTransitive = false }
-//    api("com.github.GTNewHorizons:NotEnoughItems:2.3.+:dev") { isChanging = true }
-//    api("com.github.GTNewHorizons:waila:1.6.0:dev") { isTransitive = false }
+    api("space.impact:VirtualWorld:2.0.0d.dirty:dev")
+    api("space.impact:gregtech-impact:5.09.35.31.dirty:dev") { isTransitive = false }
+    compileOnly("space.impact:OpenComputers:1.7.5.7-impact")
+
+    // maven impact
+    api("com.github.GTNewHorizons:CodeChickenCore:1.3.11:dev") {
+        version { strictly("1.3.11") }
+    }
+    api("com.github.GTNewHorizons:modularui:1.1.24:dev") { isTransitive = false }
+    api("com.github.GTNewHorizons:NotEnoughItems:2.6.0-GTNH:dev")
+    implementation("com.github.Azanor:Baubles:1.0.1.12")
+    implementation("com.github.GTNewHorizons:waila:1.7.3:dev") { isTransitive = false }
+    implementation("com.github.GTNewHorizons:EnderCore:0.2.7:dev") { isTransitive = false }
+    implementation("com.github.GTNewHorizons:appliedenergistics2:rv3-beta-400-GTNH") { isTransitive = false }
+    compileOnly("com.github.GTNewHorizons:NotEnoughEnergistics:1.3.25:dev") { isTransitive = false }
+    runtimeOnlyNonPublishable("com.github.GTNewHorizons:IC2NuclearControl:2.6.2:dev") { isTransitive = false }
+
+    // other
     api("net.industrial-craft:industrialcraft-2:2.2.828-experimental:dev")
-//    api("com.github.GTNewHorizons:inventory-tweaks:1.5.+:api") { isTransitive = false }
-//    api("com.github.GTNewHorizons:EnderCore:0.2.+:dev") { isTransitive = false }
-
-//    compileOnly("com.github.GTNewHorizons:NotEnoughEnergistics:1.4.2:dev") { isTransitive = false }
-//    runtimeOnly("com.github.GTNewHorizons:Nuclear-Control:2.4.+:dev") { isTransitive = false; isChanging = true }
-
-//    implementation("com.github.GTNewHorizons:Applied-Energistics-2-Unofficial:rv3-beta-400-GTNH") { isTransitive = false }
-    implementation(fileTree(mapOf("dir" to "libs/", "include" to listOf("*.jar"))))
-    compileOnly(fileTree(mapOf("dir" to "libs/compile", "include" to listOf("*.jar"))))
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
